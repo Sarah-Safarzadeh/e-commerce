f4fd30a127fea8c0497baa76c6a0247ba0b6a027
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-06 00:16:58 -0600

    put route to update tags name by ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -71,6 +71,22 @@ router.post("/", (req, res) => {
 // PUT ROUTE
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData[0]) {
+      res.status(404).json({ message: 'There was no tag found with this id.' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // DELETE ROUTE
